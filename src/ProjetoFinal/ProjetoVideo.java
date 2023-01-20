e4a68b2293da378e4e4b30636a91392c88f92d95
Antonio Jonilei <100094268+antoniojonilei@users.noreply.github.com>
2023-01-20 15:14:21 -0300

    projeto final

--- src/ProjetoFinal/ProjetoVideo.java
+++ src/ProjetoFinal/ProjetoVideo.java
@@ -5,23 +5,22 @@ public class ProjetoVideo {
         Video videos[] = new Video[3];
         videos[0] = new Video("Aprendendo Java");
         videos[1] = new Video("Aula 1 de POO");
-        videos[2] = new Video("Como ser Front End");
-
-        videos[0].status();
-        // videos[1].status();
-        // videos[2].status();
+        videos[2] = new Video("Como ser Front End");        
 
         Usuario usuarios[] = new Usuario[3];
         usuarios[0] = new Usuario("Jonilei", 31, "M", "jonileisilva");
         usuarios[1] = new Usuario("Kitana", 25, "F", "kitanawins");
         usuarios[2] = new Usuario("Sheeva", 60, "F", "Sheevawins");
 
+        /* 
+        videos[0].status();
+        videos[1].status();
+        videos[2].status();
+
         usuarios[0].detalhes();
         usuarios[1].detalhes();
-        usuarios[2].detalhes();
-
-        // System.out.println(videos[0].getTitulo());
-      
+        usuarios[2].detalhes(); 
+        */      
 
     }
 }
